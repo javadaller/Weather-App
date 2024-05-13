a5393bc33d2e7eaef67e5ad8a292245eb51a3d7d
javadaller <arnaudvanacker@yahoo.fr>
2024-05-13 10:52:32 +0200

    first commit

---
+++ assets/js/fnc.js
@@ -0,0 +1,45 @@
+// CREATE DIV
+export function createDiv(type,parent,content,className) {
+  const newDiv=document.createElement(type);
+  if (content!=null) {
+    newDiv.innerHTML=content;
+  }
+  if (className!=null) {
+    newDiv.classList.add(className);
+  }
+  parent.appendChild(newDiv);
+  return newDiv;
+}
+
+//------------------------------------------------------------------------------------------
+// SLEEP FUNCTION
+export function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//------------------------------------------------------------------------------------------
+// ESCAPE HTML
+export function escapeHtml(text) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+  
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+//------------------------------------------------------------------------------------------
+// IS MOBILE
+export function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+//------------------------------------------------------------------------------------------
+// RANDOM COLOR
+export function randomColor() {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  return '#'+randomColor;
+}
---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+import { sleep } from "./fnc.js";
+
+//INIT DATA
